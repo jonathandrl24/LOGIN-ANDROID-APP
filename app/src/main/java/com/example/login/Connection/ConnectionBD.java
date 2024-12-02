ed869be0109fe1192465ae0e3df8c622ab3d554f
Jonathan David Rodriguez Lozada <102917544+jonathandrl24@users.noreply.github.com>
2024-12-02 03:14:05 -0500

    Avance Conexion Base de Datos

--- app/src/main/java/com/example/login/Connection/ConnectionBD.java
+++ app/src/main/java/com/example/login/Connection/ConnectionBD.java
@@ -1,15 +1,38 @@
 package com.example.login.Connection;
 
+import android.annotation.SuppressLint;
+import android.os.StrictMode;
+
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConnectionBD {
-    private String ip = "192.168.68.105";
-    private String usuario = "root";
-    private String password = "";
-    private String basedatos = "login";
+    private final String ip = "192.168.68.105"; // Cambia a la IP correcta de tu base de datos
+    private final String usuario = "root";
+    private final String password = "";
+    private final String basedatos = "login";
 
-    public Connection Connect() {
+    @SuppressLint("NewApi")
+    public Connection connect() {
+        Connection connection = null;
+        String connectionUrl;
+        try {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
 
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connectionUrl = "jdbc:mysql://" + ip + ":3306/" + basedatos + "?useSSL=false&serverTimezone=UTC";
+
+            connection = DriverManager.getConnection(connectionUrl, usuario, password);
+        } catch (ClassNotFoundException e) {
+            System.err.println("Error: Clase del driver no encontrada. Verifica que agregaste el conector MySQL.");
+            e.printStackTrace();
+        } catch (SQLException e) {
+            System.err.println("Error: Fallo en la conexión con la base de datos. Verifica los detalles de conexión.");
+            e.printStackTrace();
+        }
+        return connection;
     }
 }
 
--- app/src/main/java/com/example/login/LoginActivity.java
+++ app/src/main/java/com/example/login/LoginActivity.java
@@ -1,9 +1,12 @@
 package com.example.login;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,7 +14,11 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.login.Connection.ConnectionBD;
+
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -19,6 +26,11 @@ public class LoginActivity extends AppCompatActivity {
  TextView lblregistrar;
  Button btnregistrar;
  Connection con;
+
+ public LoginActivity() {
+        ConnectionBD instanceConnection = new ConnectionBD();
+        con = instanceConnection.connect();
+ }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,5 +47,52 @@ public class LoginActivity extends AppCompatActivity {
         lblregistrar = (TextView) findViewById(R.id.lblregistrar);
         btnregistrar = (Button) findViewById(R.id.btningresar);
 
+    }
+
+    public class login extends AsyncTask<String, String, String> {
+     String z=null;
+     Boolean exito = false;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            if(con == null){
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(LoginActivity.this, "Verificar su conexion", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                z = "En conexion";
+            }
+            else {
+                try {
+                    String sql = "SELECT * FROM USUARIO WHERE usuario = '" + usuario.getText() + "' AND clave = '" + clave.getText() + "'";
+                    Statement stm = con.createStatement();
+                    ResultSet rs = stm.executeQuery(sql);
+
+                    if(rs.next()){
+                        Toast.makeText(LoginActivity.this, "Acceso exitoso", Toast.LENGTH_SHORT).show();
+
+                    }
+
+                }catch (Exception e){
+                    exito=false;
+                    Log.e("Error de conexion", e.getMessage());
+                }
+            }
+
+
+            
+            return z;
+        }
     }
 }
